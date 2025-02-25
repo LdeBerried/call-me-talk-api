b498c30ea763bd7361a255d5c52577380812329e
Laura <laalpri@gmail.com>
2025-02-25 22:14:29 +0100

    adding versioning api

---
+++ app/api/routers/v1/books.py
@@ -0,0 +1,101 @@
+import uuid
+
+from fastapi import Path, Query, APIRouter
+
+from app.src.domain.book import GET_BOOKS_JSON, Book
+
+GET_BOOKS_EP_DESCRIPTION = """
+## GET /books
+
+Retrieves a list of books from the library's collection.
+
+### Description
+
+This endpoint is used to fetch a list of books available in the library's collection. The collection is updated daily to ensure the most up-to-date information is provided to users. Books can be filtered and sorted using query parameters (such as title, authors, year, or format) to facilitate efficient searching and retrieval.
+
+### How It Works
+
+When a request is made to this endpoint, the server queries the database for all available books and returns a JSON array of book objects. Each book object adheres to the following `Book` schema:
+
+### Schema Details
+
+- **Book ID** `book_id`: A unique identifier for each book. This is a universally unique identifier (UUID).
+- **Title** `book_title`: The title of the book.
+- **ISBN** `isbn`: The International Standard Book Number (ISBN) that uniquely identifies the book.
+- **Publishing Year** `year`: The year when the book was first published. It can be any year up to and including 2025.
+- **Format** `book_format`: The format of the book, such as softcover or hardcover.
+- **Library Code** `library_code`: A unique code assigned by the library, must end with the character '-' followed by a number.
+- **Authors** `authors`: A list of authors who contributed to the book.
+- **Check Outs** `check_outs`: A list of instances when the book was checked out, if any.
+
+### Domain Terminology
+
+- **UUID**: Universally Unique Identifier. A 128-bit identifier used to uniquely identify information in computer systems.
+- **ISBN**: International Standard Book Number. A unique identifier for books, allowing for more efficient management and sale of books.
+- **Softcover**: A type of book binding that uses a flexible paper cover.
+- **Hardcover**: A type of book binding that uses a rigid protective cover.
+- **Audiobook**: Audible type of book avaliable for download.
+
+"""
+
+GET_BOOK_EP_DESCRIPTION = """
+## GET /books/{book_id}
+
+Retrieves a single book from the library's collection.
+### Description
+This endpoint is used to fetch a single book from the library's collection. The collection is updated daily to ensure the most up-to-date information is provided. Individual books can only be retrieved by using their unique book identifier (`book_id`).
+
+### How It Works
+When a request is made to this endpoint, the server queries the database for the specific book and returns a single book object. The book object adheres to the following `Book` schema:
+
+### Schema Details
+
+- **Book ID** `book_id`: A unique identifier for each book. This is a universally unique identifier (UUID).
+- **Title** `book_title`: The title of the book.
+- **ISBN** `isbn`: The International Standard Book Number (ISBN) that uniquely identifies the book.
+- **Publishing Year** `year`: The year when the book was first published. It can be any year up to and including 2025.
+- **Format** `book_format`: The format of the book, such as softcover or hardcover.
+- **Library Code** `library_code`: A unique code assigned by the library, must end with the character '-' followed by a number.
+- **Authors** `authors`: A list of authors who contributed to the book.
+- **Check Outs** `check_outs`: A list of instances when the book was checked out, if any.
+
+### Domain Terminology
+- **Book ID** `book_id`: Unique identifier for each book. This is a universally unique identifier (UUID).
+
+"""
+
+router = APIRouter(prefix="/v1/books", tags=["Books", "v1"])
+
+
+@router.get(
+    "/",
+    summary="Retrieve books from the library's collection",
+    description=GET_BOOKS_EP_DESCRIPTION,
+)
+async def get_books() -> list[Book]:
+    raw_books = [book for book in GET_BOOKS_JSON]
+    result = [Book.model_validate(raw_book) for raw_book in raw_books]
+    return result
+
+
+@router.get(
+    "/{book_id}",
+    summary="Retrieve a specific book from the library's collection",
+    description=GET_BOOK_EP_DESCRIPTION,
+    deprecated=True,
+)
+async def get_book(
+    book_id: uuid.UUID = Path(
+        description="A unique identifier for a book from the library's collection",
+        title="Book ID",
+        examples=["Book ID in UUID Format (eg. a358bd48-0d86-4743-988b-f81e5d88e5d7)"],
+    ),
+    showing_random: bool | None = Query(default=None, description="Show random book"),
+    hidden_random: bool | None = Query(
+        default=None,
+        description="Show random book. **Hidden Parameter**",
+        include_in_schema=False,
+    ),
+) -> Book:
+    result = Book.model_validate(GET_BOOKS_JSON[0])
+    return result
--- tests/test_get_book.py
+++ tests/test_get_book.py
@@ -13,7 +13,12 @@ class TestGetBook:
         self.base_url = "http://localhost:8000"
         self.client = TestClient(app)
 
-    def test_request_should_get_200(self):
+    def test_request_should_get_200_for_v1(self):
         book_id = uuid.uuid4()
-        response = self.client.get(f"/books/{book_id}")
+        response = self.client.get(f"/v1/books/{book_id}")
+        assert response.status_code == 200
+
+    def test_request_should_get_200_for_v2(self):
+        book_title = "Pride and Prejudice"
+        response = self.client.get(f"/v2/books/{book_title}")
         assert response.status_code == 200
